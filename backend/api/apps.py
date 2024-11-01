0ccc5c78643b3a0ebde1aab3550c381f4c54faee
Evgeny Shutov <acidic@ya.ru>
2024-11-01 16:46:19 +0300

    Instructions for flake8

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,9 @@
+"""APPS."""
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """ApoConfig."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,14 @@
+"""Serializers."""
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """Task сериалазйер."""
+
     class Meta:
+        """Класс Мета."""
+
         model = Task
         fields = ('id', 'title', 'description', 'completed')
